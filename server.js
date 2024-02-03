34db2d4def54d49a842b2765fdc958cc81126bf4
Sachin Pathak <pathaks411@gmail.com>
2024-02-03 15:36:08 +0530

    lat & long is done via socket

--- server.js
+++ server.js
@@ -2,44 +2,59 @@
 import express from "express";
 import { Server } from 'socket.io';
 const app = express();
-const port =  4000;
-const wifiIp = "192.168.1.4"
+const port = 4000;
+// const wifiIp = "192.168.1.4"
 var server = app.listen(port, () => {
     console.log(`Server is running ${port}`);
 });
 
-// const server = require('http').createServer(app);
-
-// app.use((req, resp, next) => {
-//     console.log("hiii");
-//     resp.header("Access-Control-Allow-Origin", "*",);
-//     resp.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
-//     resp.header("Access-Control-Allow-Headers", "Origin, X-Requested-With,Authorization,Content-Type,Accept");
-//     next();
-// });
-
 const io = new Server(server, {
-    cors: { origin: "*"}
+    cors: { origin: "*" }
 });
 
-app.get("/",(req,resp)=> {
+app.get("/", (req, resp) => {
     resp.status(200).json("socket is running");
 })
 
 
 io.on('connection', (socket) => {
-    console.log('connection');
+    console.log('connection establish');
+    socket.emit("connected","nodejs establish connection")
 
-    socket.on('sendChatToServer', (message) => {
-        console.log(message);
+    socket.on("trucking",(btnKaMsg)=>{
+        const room = btnKaMsg.room;
+        console.log("location",btnKaMsg);
+        io.to(room).emit("track location",btnKaMsg);
+        console.log("Done");
+    })
+    socket.on("join", (room) => {
+        socket.join(room);
 
-        io.emit('emmiting', message);
-        socket.broadcast.emit('sendChatToClient', message);
+        socket.on("new message", (message) => {
+            console.log("message of kotlin",message);
+            console.log('Message room:', room);
+            io.to(room).emit("message recieved", message);
+            console.log('Message received:', message);
+        });
+
+        socket.on("trucking", (messageobject) => {
+            console.log('Message room:', room);
+            io.to(room).emit("trucking", messageobject);
+            console.log('sdfasf', messageobject);
+        });
     });
-
+    // socket.on("new message", (message) => {
+    //     const room = Object.keys(socket.rooms)[0]; // Get the room of the current socket
+    //     console.log('Message room:', room);
+    //     socket.broadcast.to(room).emit("message received", message);
+    //     console.log('Message received:', message);
+    // });
 
     socket.on('disconnect', (socket) => {
         console.log('Disconnect');
     });
-});
+})
+
+
+
 
